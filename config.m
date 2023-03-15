0d15330491dcaae2d5d6d09ec71d41d8c6427c55
cgj <18700163154@163.com>
2023-03-15 12:01:23 +0800

    pi

--- config.m
+++ config.m
@@ -1,7 +1,7 @@
-clc; clear;close all;
+clc; clear; close all;
 conf.filename = 'data/Output_500';
-conf.start = [280 80 500 3.14 * 45/180 0 0];
-conf.goal = [70 600 600 3.14 * 45/180 0 0];
+conf.start = [280 80 500 pi * 45/180 0 0];
+conf.goal = [70 600 600 pi * 45/180 0 0];
 conf.threshold_close = 300;
 conf.threshold_goal = 6000;
 conf.search = 1.6;
@@ -13,13 +13,13 @@ conf.deltaT = 200; %s
 conf.g = 9.8;
 conf.v = 5; %m/s
 conf.acc = 999999999;
-conf.GammaMax = 30/180 * 3.1416;
-conf.GammaMin = -30/180 * 3.1416;
-conf.pitchMax = 30/180 * 3.1416;
-conf.pitchMin = -30/180 * 3.1416;
+conf.GammaMax = 30/180 * pi;
+conf.GammaMin = -30/180 * pi;
+conf.pitchMax = 30/180 * pi;
+conf.pitchMin = -30/180 * pi;
 
-conf.GammaStep = 15/180 * 3.1416; %滚转角最大步长
-conf.pitchstep = 10/180 * 3.1416; %俯仰角最大步长
+conf.GammaStep = 15/180 * pi; %滚转角最大步长
+conf.pitchstep = 10/180 * pi; %俯仰角最大步长
 stable_distance = 'data/mini_stable_distance';
 load(stable_distance, 'mini_stable_distance')
 conf.mini_stable_distance = mini_stable_distance;
@@ -28,7 +28,7 @@ r = rrt(conf);
 r.start_star(true, 200, 0.01);
 
 % %列名称
-% col={ 'x', 'y', 'z'}; 
+% col={ 'x', 'y', 'z'};
 % %生成表格，按列生成
 % result_table=table(out(:,1),out(:,2),out(:,3),'VariableNames',col);
 % %保存表格
