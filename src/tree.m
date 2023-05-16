67a81dd5b1322143385186e359417bb4120f1ef5
cgj <18700163154@163.com>
2023-05-16 20:00:53 +0800

    fix change_parent bug

--- src/tree.m
+++ src/tree.m
@@ -20,33 +20,30 @@ classdef tree < handle
             this.preorder(1, 1) = 1;
         end
 
-        function next_id = next_preorder_id(this, id)
+        function next_id = next_peer_id(this, id)
             %get next peer's id or find parent's if none
             parent_id = this.parent(id, 1);
-            peers = this.children{parent_id};
-            ind_self = find(peers == id);
 
-            if isempty(peers)
+            if parent_id == -1
                 next_id = 0;
                 return
             end
+
+            peers = this.children{parent_id};
+            ind_self = find(peers == id);
 
             if ind_self < length(peers)
                 next_id = peers(ind_self + 1);
             elseif ind_self == length(peers) %is last one
 
-                if parent_id ~= this.preorder(1, 1)
-                    next_id = this.next_preorder_id(parent_id);
-                else %end of the preorder
-                    next_id = 0;
-                end
+                next_id = this.next_peer_id(parent_id);
 
             end
 
         end
 
         function next_ind = next_preorder_ind(this, id)
-            next_id = this.next_preorder_id(id);
+            next_id = this.next_peer_id(id);
 
             if next_id
                 next_ind = find(this.preorder == next_id);
@@ -78,17 +75,18 @@ classdef tree < handle
 
         function change_parent(this, id, new_parent_id)
             ind_all = find(ismember(this.preorder, id));
+            id_all = this.preorder(ind_all, 1); %ordered id
 
-            for i = length(ind_all):-1:1 %change in inverse order
-                ind = ind_all(i);
-                self_id = this.preorder(ind);
+            for i = length(id_all):-1:1 %change in inverse order
+                self_id = id_all(i);
+                ind = find(this.preorder == self_id);
                 next_ind = this.next_preorder_ind(self_id); %next index of last offspring in preorder array
                 insert_ind = this.next_preorder_ind(new_parent_id); %insert index
 
                 if ind < insert_ind
                     this.preorder = [this.preorder(1:ind - 1); this.preorder(next_ind:insert_ind - 1); this.preorder(ind:next_ind - 1); this.preorder(insert_ind:end)];
                 else
-                    this.preorder = [this.preorder(1:insert_ind - 1); this.preorder(ind:next_ind - 1); this.preorder(insert_ind + 1:ind - 1); this.preorder(next_ind:end)];
+                    this.preorder = [this.preorder(1:insert_ind - 1); this.preorder(ind:next_ind - 1); this.preorder(insert_ind:ind - 1); this.preorder(next_ind:end)];
                 end
 
                 old_parent_id = this.parent(self_id);
